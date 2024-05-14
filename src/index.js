13901cc1e908aabc562f81a683fdbc392335431a
Adi6360 <107831411+Adi6360@users.noreply.github.com>
2024-05-14 21:33:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import {store} from './app/store';
 import {provider} from 'react-redux';
 import { disableReactDevTools} from '@fvilers/disable-react-devtools'; 
 
@@ -14,6 +13,6 @@ root.render(
     <>
       <App />
     </>
-  </Suspense>,
+  </Suspense>
 );
 
