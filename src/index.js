a7b1a88363179e7d4b53316f5ee44041a8967b32
Adi6360 <107831411+Adi6360@users.noreply.github.com>
2024-05-14 22:31:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import {provider} from 'react-redux';
+//import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { disableReactDevTools} from '@fvilers/disable-react-devtools'; 
 
 if (process.env.NODE_ENV === 'production') disableReactDevTools()
