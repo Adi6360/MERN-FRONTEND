12e25bd5f5a7ba2207dcbb5ae8f5d2d5cd93191f
Adi6360 <107831411+Adi6360@users.noreply.github.com>
2024-05-15 13:03:18 +0530

    Update apiSlice.js

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({ 
-  baseUrl: 'https://backend-api.onrender.com' });
+  baseUrl: 'https://backend-api-vveg.onrender.com' });
 
 export const apiSlice = createApi({
   baseQuery,
